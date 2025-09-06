1f1a8402e2785e4adf9beefb97c6245cbdebe18d
Tanja Mueller <tmueller19@gmx.de>
2025-09-06 16:37:06 +0200

    your turn

--- index.js
+++ index.js
@@ -31,29 +31,31 @@ else if (hacker1.length === hacker2.length) {
 // Iteration 3: Loops
 
 // 3.1
-const driver = hacker1[0] + " " + hacker1[1].toUpperCase() + " " + hacker1[2].toUpperCase();
-console.log(driver);
+const driverSpace = hacker1[0] + " " + hacker1[1].toUpperCase() + " " + hacker1[2].toUpperCase();
+console.log(driverSpace);
 
 // 3.2
-const driverRev = hacker1[2] + hacker1[1] + hacker1[0];
-console.log(driverRev); 
+/* const driverRev = hacker1[2] + hacker1[1] + hacker1[0];
+console.log(driverRev);  */
+let driverRev = ""
+
+for (let i = hacker1.length - 1; i >= 0; i--) {
+    driverRev += hacker1[i];
+}
+
+console.log(driverRev);
 
 // 3.3
 
-if (hacker1 > hacker2){
+const driver = "Tanja";
+const navigator = "Amr";
 
+if (driver.localeCompare(navigator) > 0) {
+    console.log(`Yo, the navigator goes first, definitely.`); 
+
+} else if (navigator.localeCompare(driver) < 0) {
     console.log(`The driver's name goes first.`);
 
-}
-
-else if(hacker1 < hacker2) {
-    
-    console.log(`Yo, the navigator goes first, definitely.`);
-
-}
-
-else if (hacker1 = hacker2) {
-
+} else if (driver.localeCompare(navigator) === 0) {
    console.log(`What?! You both have the same name?`);
-
 }
